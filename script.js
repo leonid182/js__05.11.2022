190f368b365e0f52e19b7201122eabfda9adcb0b
leonid <blink_182@inbox.ru>
2022-11-05 13:49:30 +0300

    Рассмотрел if

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-if(5>4){
-    console.log('5 больше 4')
+const gender = prompt('what is your gender');
+
+if(gender.toLocaleLowerCase()==='муж'){
+    console.log('Вам налево')
+}
+if(gender.toLocaleLowerCase()==='жен'){
+    console.log('Вам направо')
 }
