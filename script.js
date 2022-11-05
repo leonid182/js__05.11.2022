adbf91819de4f41fde30c86528d554ff354c80c4
leonid <blink_182@inbox.ru>
2022-11-05 14:08:10 +0300

    Add else if

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const gender = 'муж';
-const age = 18;
+let age;
 
 
 if(age>18){
     console.log('вам больше 18')
 }else if (age<18){
     console.log('вам меньше 18')
+}else if(age===undefined){
+    console.log('введите возраст')
 }else{
-    console.log('congraulation')
+    console.log('вам 18')
 }
 
 
