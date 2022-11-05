e12e4cebd6543651d88f1d22a81298f78c4bb2e0
leonid <blink_182@inbox.ru>
2022-11-05 14:00:08 +0300

    hahaha

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// const gender;
-const age = 19;
+const gender = 'муж';
+const age = 18;
 
 
 if(age>18){
     console.log('вам больше 18')
 }else if (age<18){
     console.log('вам меньше 18')
+}else{
+    console.log('congraulation')
 }
 
 
-// if(gender.toLocaleLowerCase()==='муж'){
-//     console.log('Вам налево')
-// }
-// if(gender.toLocaleLowerCase()==='жен'){
-//     console.log('Вам направо')
-// }
+if(gender.toLocaleLowerCase()==='муж'){
+    console.log('Вам налево')
+}
+if(gender.toLocaleLowerCase()==='жен'){
+    console.log('Вам направо')
+}
