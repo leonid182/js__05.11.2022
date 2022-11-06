46132cb21bd22d54c0ff23ae23117eec208f3e4b
leonid <blink_182@inbox.ru>
2022-11-06 22:14:40 +0300

    Система рекомендаций с температурой

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let a=10;
-let b=20;
-let c=30;
+let t=22;
 
- (a>b&&a>c)? alert(a): (b>a&&b>c)? alert(b):alert(c);
+if(t<= -30){
+    console.log('Stay home');
+}
+else if( t>-30&&t<=-10) {
+    console.log('it is cold today');
+}
+else if(t>-10&&t<=-5){
+    console.log('Not cold');
+}
+else if(t>5&&t<=15){
+    console.log('Warm');
+}
+else if(t>15&&t<=25){
+    console.log('Vary warm');
+}
+else if(t>25&&t<=35){
+    console.log('Hot');
+}
+else if(t>35){
+    console.log('Hell');
+}
+
 
 
