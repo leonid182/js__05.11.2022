cc80fe63f4d9572fd1a193685162ef5f61dfe97d
leonid <blink_182@inbox.ru>
2022-11-07 22:06:10 +0300

    Работа с && (и) и || (или)

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let test = true;
+let a =3;
+let b =5;
 
-// if(test == true){
+// if(a>0&&a<5){
 //     console.log('correct');
 // }else{
 //     console.log('incorrect');
 // }
 
 
-test == true ? console.log('correct') : console.log('incorrect');
+// if(a==0|| a==2){
+//     console.log(a+7);
+// }else{
+//     console.log(a/10);
+// }
+
+
+if(a<=1 && b>=3){
+    console.log(a+b);
+}else{
+    console.log(a-b);
+}
+
+
+
+
 
 
 
